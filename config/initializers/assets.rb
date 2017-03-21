ec67fd7bac9179625bcc2cf84a090e620ea14304
RachelMinto <rachel.j.minto@gmail.com>
2017-03-20 20:11:10 -0400

    Necessary to have application.js include bootstrap-sprockets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.assets.precompile += ['star_rating.js', 'bootstrap.js']
+Rails.application.config.assets.precompile += ['star_rating.js']
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
