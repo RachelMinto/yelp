96a6e73b3646b6b192a2c4621d7f7dc597aec542
RachelMinto <rachel.j.minto@gmail.com>
2017-03-20 20:40:36 -0400

    Include tag for star rating.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.config.assets.precompile += %w( bootstrap-sprockets.js star-rating.js)
+Rails.application.config.assets.precompile += %w( bootstrap-sprockets.js)
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
